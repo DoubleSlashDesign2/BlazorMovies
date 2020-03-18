95c659b737c2d38c82452b85b93a29e0f60906a1
Larry Meadows <LMeadows@myaccessone.com>
2020-03-18 17:34:04 -0400

    DI and Lifecycle info

--- Client/Program.cs
+++ Client/Program.cs
@@ -14,7 +14,18 @@ namespace BlazorMovies.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            ConfigureServices(builder.Services); // configured our services with DependencyInjection
+
             await builder.Build().RunAsync();
         }
+
+        // configured our services with DependencyInjection - on client site used program.cs class on server use startup.cs
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions(); // Used for - Authorization System 
+            services.AddSingleton<SingletonService>();
+            services.AddTransient<Transientservice>();
+        }
+
     }
 }
