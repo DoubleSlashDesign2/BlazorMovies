e27e6a8f1cda7a07650b45d8203fb253ed67f073
Larry Meadows <DoubleSlashDesign@gmail.com>
2020-03-30 11:18:20 -0400

    todos

---
+++ Client/Model/TodoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Model
+{
+    public class TodoModel
+    {
+        // -- Add model to _Imports.razor to make available global
+        public string Text { get; set; }
+        public bool Done { get; set; }
+    }
+}
